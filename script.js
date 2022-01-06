3c95aa29fa30fe8b9d1f5be7dbcfb37270110a0c
Andrew Garcia <andrewagarcia8@gmail.com>
2022-01-06 11:31:26 -0800

    update

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var symbols ="!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
-var uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowercase ='abcdefghijklmnopqrstuvwxyz';
-var num = '0123456789';
-
-var passwordChar = [symbols, uppercase, lowercase, num];
-
-  console.log(passwordChar)
 
 function generatePassword() {
+
+  var characters ="!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz123456789";
     
-    // 1. Prompt the user for the password criteria
     var save_user_input = prompt("How many characters would you like your password? ( Between 8 and 128)")
    
-    // a. Password lenght 8 < 128
     if (save_user_input < 8) {
       alert("This is less than 8")
+      return "invalid input"
     } else if (save_user_input >= 8 && save_user_input <= 128) {
-      alert("Would you like to include symbols?")
+      var includeNum = confirm("Would you like to include numbers?")
+      console.log(includeNum)
     } else {
       alert("This is more than 128")
+      return "invalid input"
     }
 
-    alert ('"Would you like to include numbers?"')
-    alert ('"Would you like to include Uppercase?"')
-    alert ('"Would you like to include Lowercase?"')
 
-  }
+     var includeSym = confirm ('"Would you like to include symbols?"')
+          console.log(includeSym)
+     var includeUpper = confirm ('"Would you like to include Uppercase?"')
+          console.log(includeUpper)
 
-      
+    var includeLower = confirm ('"Would you like to include Lowercase?"')
+          console.log(includeLower)
 
-    //      b. lowercase, Uppercase, numbers, special characters
-    // 2. Validate the input
-    // 3. Display password to the page
+    var password = ""
+    
+    for (var i=0; i <= save_user_input; i++) {
+        password += characters.chartAt(Math.floor(Math.random() * characters.length));
+        
+    }
 
+}
 
 // Write password to the #password input
 function writePassword() {
