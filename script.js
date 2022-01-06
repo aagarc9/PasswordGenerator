68350fb9a779d04c785adc74c1abc2fa31db46c6
Andrew Garcia <andrewagarcia8@gmail.com>
2022-01-05 16:48:43 -0800

    Updated

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ var uppercase ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 var lowercase ='abcdefghijklmnopqrstuvwxyz';
 var num = '0123456789';
 
+var passwordChar = [symbols, uppercase, lowercase, num];
+
+  console.log(passwordChar)
+
 function generatePassword() {
     
     // 1. Prompt the user for the password criteria
     var save_user_input = prompt("How many characters would you like your password? ( Between 8 and 128)")
-
+   
+    // a. Password lenght 8 < 128
     if (save_user_input < 8) {
       alert("This is less than 8")
     } else if (save_user_input >= 8 && save_user_input <= 128) {
@@ -19,11 +24,14 @@ function generatePassword() {
       alert("This is more than 128")
     }
 
-}
+    alert ('"Would you like to include numbers?"')
+    alert ('"Would you like to include Uppercase?"')
+    alert ('"Would you like to include Lowercase?"')
+
+  }
+
       
 
-
-    //      a. Password lenght 8 < 128
     //      b. lowercase, Uppercase, numbers, special characters
     // 2. Validate the input
     // 3. Display password to the page
